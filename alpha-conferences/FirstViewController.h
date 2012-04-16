cb3bff32f57c947a3118405371fc7852a8a073a1
Cameron Cooke <cameron@brightec.co.uk>
2012-04-16 10:08:13 +0100

    Initial Commit

---
+++ alpha-conferences/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  alpha-conferences
+//
+//  Created by Cameron Cooke on 16/04/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ alpha-conferences/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  alpha-conferences
+//
+//  Created by Cameron Cooke on 16/04/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
