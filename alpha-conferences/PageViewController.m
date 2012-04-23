7feba29ed55d25e7096698283a9f399a35d30377
Erik Erskine <erik@brightec.co.uk>
2012-04-23 16:37:09 +0100

    new PageViewController for showing small bits of HTML

---
+++ alpha-conferences/PageViewController.m
@@ -0,0 +1,69 @@
+//
+//  PageViewController.m
+//  alpha-conferences
+//
+//  Created by Erik Erskine on 23/04/2012.
+//  Copyright (c) 2012 Brightec Ltd. All rights reserved.
+//
+
+#import "PageViewController.h"
+
+
+@interface PageViewController ()
+
+@property (nonatomic, strong, readonly) NSString *pageTitle;
+@property (nonatomic, strong, readonly) NSString *pageContent;
+
+@end
+
+
+
+@implementation PageViewController
+
+@synthesize pageTitle = _pageTitle;
+@synthesize pageContent = _pageContent;
+
+
+- (void)dealloc {
+    ((UIWebView *)self.view).delegate = nil;
+}
+
+
+- (id)initWithPageTitle:(NSString *)pageTitle pageContent:(NSString *)pageContent {
+    if (self = [super initWithNibName:nil bundle:nil]) {
+        _pageTitle = pageTitle;
+        _pageContent = pageContent;
+    }
+    return self;
+}
+
+
+- (void)loadView {
+    UIWebView *wv = [[UIWebView alloc] initWithFrame:[[UIScreen mainScreen] applicationFrame]];
+    wv.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    wv.delegate = self;
+    self.view = wv;
+}
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    UIWebView *wv = (UIWebView *)self.view;
+    NSString *css = @"body { font-family: Helvetica; } h1 { font-size: 18px; }";
+    NSString *html = [NSString stringWithFormat:@"<!DOCTYPE html><html><head><style type='text/css'>%@</style><body><h1>%@</h1>%@</body></html>", css, self.pageTitle, self.pageContent];
+    [wv loadHTMLString:html baseURL:nil];
+}
+
+
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
+    if (navigationType == UIWebViewNavigationTypeLinkClicked) {
+        [[UIApplication sharedApplication] openURL:request.URL];
+        return NO;
+    }
+    else {
+        return YES;
+    }
+}
+
+
+@end
