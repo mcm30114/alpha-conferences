d6c94fb423c011d6c5b8042a4592ae9d84be1e30
Jordan de Laune <jordan.delaune@gmail.com>
2012-04-23 14:11:35 +0100

    Add APNS receiver
    Action needs to be added to take user to the alert view

--- alpha-conferences/AppDelegate.m
+++ alpha-conferences/AppDelegate.m
@@ -122,7 +122,10 @@
     tabController.viewControllers = tabControllers;
     self.tabBarController = tabController;
     self.tabBarController.delegate = self;
-  
+
+    [[UIApplication sharedApplication] registerForRemoteNotificationTypes:(UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound)];
+    application.applicationIconBadgeNumber = 0;
+
     self.window.rootViewController = self.tabBarController;
     [self.window makeKeyAndVisible];
     [DataStore refresh];
@@ -154,6 +157,7 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    [DataStore refresh];
 }
 
 
@@ -161,5 +165,121 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)devToken
+{
+#if !TARGET_IPHONE_SIMULATOR
+  
+	NSString *appName = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleDisplayName"];
+	NSString *appVersion = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"];
+	NSUInteger rntypes = [[UIApplication sharedApplication] enabledRemoteNotificationTypes];
+  
+	NSString *pushBadge = @"disabled";
+	NSString *pushAlert = @"disabled";
+	NSString *pushSound = @"disabled";
+  
+	if(rntypes == UIRemoteNotificationTypeBadge){
+		pushBadge = @"enabled";
+	}
+	else if(rntypes == UIRemoteNotificationTypeAlert){
+		pushAlert = @"enabled";
+	}
+	else if(rntypes == UIRemoteNotificationTypeSound){
+		pushSound = @"enabled";
+	}
+	else if(rntypes == ( UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeAlert)){
+		pushBadge = @"enabled";
+		pushAlert = @"enabled";
+	}
+	else if(rntypes == ( UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound)){
+		pushBadge = @"enabled";
+		pushSound = @"enabled";
+	}
+	else if(rntypes == ( UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeSound)){
+		pushAlert = @"enabled";
+		pushSound = @"enabled";
+	}
+	else if(rntypes == ( UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeSound)){
+		pushBadge = @"enabled";
+		pushAlert = @"enabled";
+		pushSound = @"enabled";
+	}
+  
+	UIDevice *dev = [UIDevice currentDevice];
+  NSString *deviceName = dev.name;
+	NSString *deviceModel = dev.model;
+	NSString *deviceSystemVersion = dev.systemVersion;
+  
+	NSString *deviceToken = [[[[devToken description]
+                             stringByReplacingOccurrencesOfString:@"<"withString:@""]
+                            stringByReplacingOccurrencesOfString:@">" withString:@""]
+                           stringByReplacingOccurrencesOfString: @" " withString: @""];
+  
+	NSString *host = @"apns.alpha.org";
+	NSString *urlString = [@"/?"stringByAppendingString:@"task=register"];
+  
+	urlString = [urlString stringByAppendingString:@"&appname="];
+	urlString = [urlString stringByAppendingString:appName];
+	urlString = [urlString stringByAppendingString:@"&appversion="];
+	urlString = [urlString stringByAppendingString:appVersion];
+	urlString = [urlString stringByAppendingString:@"&devicetoken="];
+	urlString = [urlString stringByAppendingString:deviceToken];
+	urlString = [urlString stringByAppendingString:@"&devicename="];
+	urlString = [urlString stringByAppendingString:deviceName];
+	urlString = [urlString stringByAppendingString:@"&devicemodel="];
+	urlString = [urlString stringByAppendingString:deviceModel];
+	urlString = [urlString stringByAppendingString:@"&deviceversion="];
+	urlString = [urlString stringByAppendingString:deviceSystemVersion];
+	urlString = [urlString stringByAppendingString:@"&pushbadge="];
+	urlString = [urlString stringByAppendingString:pushBadge];
+	urlString = [urlString stringByAppendingString:@"&pushalert="];
+	urlString = [urlString stringByAppendingString:pushAlert];
+	urlString = [urlString stringByAppendingString:@"&pushsound="];
+	urlString = [urlString stringByAppendingString:pushSound];
+  
+	NSURL *url = [[NSURL alloc] initWithScheme:@"http" host:host path:urlString];
+  NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url];
+	[NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+  
+#endif
+}
+
+/**
+ * Failed to Register for Remote Notifications
+ */
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+	NSLog(@"Error in registration. Error: %@", error);
+}
+
+/**
+ * Remote Notification Received while application was open.
+ */
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+#if !TARGET_IPHONE_SIMULATOR
+    [DataStore refresh];
+    NSDictionary *apsInfo = [userInfo objectForKey:@"aps"];
+    NSString *alert = [apsInfo objectForKey:@"alert"];
+    NSNumber *alertId = [userInfo objectForKey:@"alertId"];
+    
+    NSLog(@"Alert Identifer: %@", alertId);
+  
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Conference Alert"
+                                                        message:alert
+                                                       delegate:self
+                                              cancelButtonTitle:@"Dismiss"
+                                              otherButtonTitles:@"Read more", nil];
+    [alertView show];
+#endif
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+  NSString *title = [alertView buttonTitleAtIndex:buttonIndex];
+  if([title isEqualToString:@"Read more"])
+  {
+    // SEND USER TO ALERTS VIEW
+  }
+}
 
 @end
