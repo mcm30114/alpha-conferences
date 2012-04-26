7fef27afff4b0a8088f17e9d80f7bcf74b7ef8f6
Jordan de Laune <jordan.delaune@gmail.com>
2012-04-26 12:31:30 +0100

    Improved APNS implementation

--- alpha-conferences/AppDelegate.m
+++ alpha-conferences/AppDelegate.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2012 Brightec Ltd. All rights reserved.
 //
 
+#import <AudioToolbox/AudioToolbox.h>
 #import "AppDelegate.h"
 #import "Programme.h"
 #import "SpeakerList.h"
@@ -221,44 +222,28 @@
 
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)devToken
 {
-#if !TARGET_IPHONE_SIMULATOR
+  #if !TARGET_IPHONE_SIMULATOR
   
 	NSString *appName = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleDisplayName"];
 	NSString *appVersion = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"];
 	NSUInteger rntypes = [[UIApplication sharedApplication] enabledRemoteNotificationTypes];
   
-	NSString *pushBadge = @"disabled";
-	NSString *pushAlert = @"disabled";
-	NSString *pushSound = @"disabled";
-  
-	if(rntypes == UIRemoteNotificationTypeBadge){
-		pushBadge = @"enabled";
-	}
-	else if(rntypes == UIRemoteNotificationTypeAlert){
-		pushAlert = @"enabled";
-	}
-	else if(rntypes == UIRemoteNotificationTypeSound){
-		pushSound = @"enabled";
-	}
-	else if(rntypes == ( UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeAlert)){
-		pushBadge = @"enabled";
-		pushAlert = @"enabled";
-	}
-	else if(rntypes == ( UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound)){
-		pushBadge = @"enabled";
-		pushSound = @"enabled";
-	}
-	else if(rntypes == ( UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeSound)){
-		pushAlert = @"enabled";
-		pushSound = @"enabled";
-	}
-	else if(rntypes == ( UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeSound)){
-		pushBadge = @"enabled";
-		pushAlert = @"enabled";
-		pushSound = @"enabled";
-	}
+	NSString *pushBadge = (rntypes & UIRemoteNotificationTypeBadge) ? @"enabled" : @"disabled";
+	NSString *pushAlert = (rntypes & UIRemoteNotificationTypeAlert) ? @"enabled" : @"disabled";
+	NSString *pushSound = (rntypes & UIRemoteNotificationTypeSound) ? @"enabled" : @"disabled";	
   
 	UIDevice *dev = [UIDevice currentDevice];
+  NSString *deviceUuid;
+  NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+  id uuid = [defaults objectForKey:@"deviceUuid"];
+  if (uuid)
+    deviceUuid = (NSString *)uuid;
+  else {
+    CFStringRef cfUuid = CFUUIDCreateString(NULL, CFUUIDCreate(NULL));
+    deviceUuid = (__bridge NSString *)cfUuid;
+    CFRelease(cfUuid);
+    [defaults setObject:deviceUuid forKey:@"deviceUuid"];
+  }
   NSString *deviceName = dev.name;
 	NSString *deviceModel = dev.model;
 	NSString *deviceSystemVersion = dev.systemVersion;
@@ -269,32 +254,16 @@
                            stringByReplacingOccurrencesOfString: @" " withString: @""];
   
 	NSString *host = @"apns.alpha.org";
-	NSString *urlString = [@"/?"stringByAppendingString:@"task=register"];
-  
-	urlString = [urlString stringByAppendingString:@"&appname="];
-	urlString = [urlString stringByAppendingString:appName];
-	urlString = [urlString stringByAppendingString:@"&appversion="];
-	urlString = [urlString stringByAppendingString:appVersion];
-	urlString = [urlString stringByAppendingString:@"&devicetoken="];
-	urlString = [urlString stringByAppendingString:deviceToken];
-	urlString = [urlString stringByAppendingString:@"&devicename="];
-	urlString = [urlString stringByAppendingString:deviceName];
-	urlString = [urlString stringByAppendingString:@"&devicemodel="];
-	urlString = [urlString stringByAppendingString:deviceModel];
-	urlString = [urlString stringByAppendingString:@"&deviceversion="];
-	urlString = [urlString stringByAppendingString:deviceSystemVersion];
-	urlString = [urlString stringByAppendingString:@"&pushbadge="];
-	urlString = [urlString stringByAppendingString:pushBadge];
-	urlString = [urlString stringByAppendingString:@"&pushalert="];
-	urlString = [urlString stringByAppendingString:pushAlert];
-	urlString = [urlString stringByAppendingString:@"&pushsound="];
-	urlString = [urlString stringByAppendingString:pushSound];
-  
-	NSURL *url = [[NSURL alloc] initWithScheme:@"http" host:host path:urlString];
+	NSString *urlString = [NSString stringWithFormat:@"/index.php?task=%@&appname=%@&appversion=%@&deviceuid=%@&devicetoken=%@&devicename=%@&devicemodel=%@&deviceversion=%@&pushbadge=%@&pushalert=%@&pushsound=%@&clientid=%d", @"register", appName,appVersion, deviceUuid, deviceToken, deviceName, deviceModel, deviceSystemVersion, pushBadge, pushAlert, pushSound, CONFERENCE_ID];
+	
+	// Register the Device Data
+	NSURL *url = [[NSURL alloc] initWithScheme:@"http" host:host path:[urlString stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
   NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url];
-	[NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+	NSData *returnData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
   
-#endif
+  NSLog(@"%@", urlString);
+  
+  #endif
 }
 
 /**
@@ -310,32 +279,35 @@
  */
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
 {
-#if !TARGET_IPHONE_SIMULATOR
-    [DataStore refresh];
-    NSDictionary *apsInfo = [userInfo objectForKey:@"aps"];
-    NSString *alert = [apsInfo objectForKey:@"alert"];
-    NSNumber *alertId = [userInfo objectForKey:@"alertId"];
-    
-    self.alertViewController = [[AlertPopupController alloc] initWithAlertId:[alertId integerValue]];
-    self.alertViewController.title = @"Alert";
-    
-    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Conference Alert"
-                                                        message:alert
-                                                       delegate:self
-                                              cancelButtonTitle:@"Dismiss"
-                                              otherButtonTitles:@"Read more", nil];
-    [alertView show];
-#endif
+  #if !TARGET_IPHONE_SIMULATOR
+  [DataStore refresh];
+  NSDictionary *apsInfo = [userInfo objectForKey:@"aps"];
+  NSString *alert = [apsInfo objectForKey:@"alert"];
+  NSNumber *alertId = [userInfo objectForKey:@"alertId"];
+  
+  AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
+  
+  self.alertViewController = [[AlertPopupController alloc] initWithAlertId:[alertId integerValue]];
+  self.alertViewController.title = @"Alert";
+  
+  NSLog(@"%d", [alertId integerValue]);
+  
+  UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Conference Alert"
+                                                      message:alert
+                                                     delegate:self
+                                            cancelButtonTitle:@"Dismiss"
+                                            otherButtonTitles:@"Read more", nil];
+  [alertView show];
+  #endif
 }
 
 - (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
-    NSString *title = [alertView buttonTitleAtIndex:buttonIndex];
-    if ([title isEqualToString:@"Read more"]) {
-        UINavigationController *popupNavController = [[UINavigationController alloc] initWithRootViewController:self.alertViewController];
-        popupNavController.navigationBar.tintColor = [UIColor navigationBarTintColour];
-        [self.tabBarController presentModalViewController:popupNavController animated:YES];
-    }
+  NSString *title = [alertView buttonTitleAtIndex:buttonIndex];
+  if ([title isEqualToString:@"Read more"]) {
+    UINavigationController *popupNavController = [[UINavigationController alloc] initWithRootViewController:self.alertViewController];
+    popupNavController.navigationBar.tintColor = [UIColor navigationBarTintColour];
+    [self.tabBarController presentModalViewController:popupNavController animated:YES];
+  }
 }
-
 
 @end
