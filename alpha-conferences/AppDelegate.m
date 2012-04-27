b09fb881b30245b8108f462be2f56c54153947fc
Erik Erskine <erik@brightec.co.uk>
2012-04-27 12:43:36 +0100

    tab order is now persisted using NSUserDefaults

--- alpha-conferences/AppDelegate.m
+++ alpha-conferences/AppDelegate.m
@@ -21,6 +21,19 @@
 #import "HomeController.h"
 #import "TwitterFeed.h"
 #import "TwitterController.h"
+#import "NSDictionary+Alpha.h"
+
+#define kTabOrder @"TAB_ORDER"
+#define kTabHome 0
+#define kTabSpeakers 1
+#define kTabProgramme 2
+#define kTabMaps 3
+#define kTabTwitter 4
+#define kTabDonate 5
+#define kTabAlerts 6
+#define kTabFAQs 7
+#define kTabOtherEvents 8
+#define kTabSpecialOffers 9
 
 
 @implementation AppDelegate
@@ -35,100 +48,120 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     NSTimeZone.defaultTimeZone = [NSTimeZone timeZoneWithAbbreviation:@"UTC"];
     
-    NSMutableArray *tabControllers = [NSMutableArray  array];
+    NSMutableDictionary *tabControllers = [NSMutableDictionary dictionary];
 
     // home
     HomeController *homeController = [[HomeController alloc] initForHome];
     homeController.title = CONFERENCE_TITLE;
     UINavigationController *homeNavController = [[UINavigationController alloc] initWithRootViewController:homeController];
     homeNavController.tabBarItem.title = @"Home";
+    homeNavController.tabBarItem.tag = kTabHome;
     homeNavController.tabBarItem.image = [UIImage imageNamed:@"home.png"];
     homeNavController.navigationBar.tintColor = [UIColor navigationBarTintColour];
-    [tabControllers addObject:homeNavController]; 
+    [tabControllers setObject:homeNavController forIntegerKey:kTabHome];
     
     // speakers
     StandardController *speakersController = [[StandardController alloc] initWithStyle:UITableViewStylePlain pager:NO];
     speakersController.model = [[SpeakerList alloc] init];
     speakersController.title = @"Speakers";
+    speakersController.tabBarItem.tag = kTabSpeakers;
     speakersController.tabBarItem.image = [UIImage imageNamed:@"speakers.png"];
     UINavigationController *speakersNavController = [[UINavigationController alloc] initWithRootViewController:speakersController];
     speakersNavController.navigationBar.tintColor = [UIColor navigationBarTintColour];
-    [tabControllers addObject:speakersNavController]; 
+    [tabControllers setObject:speakersNavController forIntegerKey:kTabSpeakers];
 
     // programme
     StandardController *programmeController = [[StandardController alloc] initWithStyle:UITableViewStylePlain pager:YES];
     programmeController.model = [[Programme alloc] init];
     programmeController.title = @"Programme";
+    programmeController.tabBarItem.tag = kTabProgramme;
     programmeController.tabBarItem.image = [UIImage imageNamed:@"programme.png"];
     UINavigationController *programmeNavController = [[UINavigationController alloc] initWithRootViewController:programmeController];
     programmeNavController.navigationBar.tintColor = [UIColor navigationBarTintColour];
-    [tabControllers addObject:programmeNavController];
+    [tabControllers setObject:programmeNavController forIntegerKey:kTabProgramme];
     
     // maps
     StandardController *mapsController = [[StandardController alloc] initWithStyle:UITableViewStylePlain pager:NO];
     mapsController.model = [[VenueListModel alloc] init];
     mapsController.title = @"Maps";
+    mapsController.tabBarItem.tag = kTabMaps;
     mapsController.tabBarItem.image = [UIImage imageNamed:@"maps.png"];
     UINavigationController *mapsNavController = [[UINavigationController alloc] initWithRootViewController:mapsController];
     mapsNavController.navigationBar.tintColor = [UIColor navigationBarTintColour];
-    [tabControllers addObject:mapsNavController];
+    [tabControllers setObject:mapsNavController forIntegerKey:kTabMaps];
 
     // twitter
     TwitterController *twitterController = [[TwitterController alloc] initWithNibName:nil bundle:nil];
     twitterController.title = @"Twitter Stream";
+    twitterController.tabBarItem.tag = kTabTwitter;
     twitterController.tabBarItem.image = [UIImage imageNamed:@"twitter.png"];
     UINavigationController *twitterNavController = [[UINavigationController alloc] initWithRootViewController:twitterController];
     twitterNavController.navigationBar.tintColor = [UIColor navigationBarTintColour];
-    [tabControllers addObject:twitterNavController];
+    [tabControllers setObject:twitterNavController forIntegerKey:kTabTwitter];
     
     // donate
     StandardController *donateController = [[StandardController alloc] initWithStyle:UITableViewStyleGrouped pager:NO];
     donateController.model = [[Donate alloc] init];
     donateController.title = @"Donate";
+    donateController.tabBarItem.tag = kTabDonate;
     donateController.tabBarItem.image = [UIImage imageNamed:@"donate.png"];
     UINavigationController *donateNavController = [[UINavigationController alloc] initWithRootViewController:donateController];
     donateNavController.navigationBar.tintColor = [UIColor navigationBarTintColour];
-    [tabControllers addObject:donateNavController];
+    [tabControllers setObject:donateNavController forIntegerKey:kTabDonate];
     
     // alerts
     StandardController *alertsController = [[StandardController alloc] initWithStyle:UITableViewStylePlain pager:NO];
     alertsController.model = [[AlertList alloc] init];
     alertsController.title = @"Alerts";
+    alertsController.tabBarItem.tag = kTabAlerts;
     alertsController.tabBarItem.image = [UIImage imageNamed:@"alerts.png"];
     UINavigationController *alertsNavController = [[UINavigationController alloc] initWithRootViewController:alertsController];
     alertsNavController.navigationBar.tintColor = [UIColor navigationBarTintColour];
-    [tabControllers addObject:alertsNavController];
+    [tabControllers setObject:alertsNavController forIntegerKey:kTabAlerts];
     
     // faqs
     StandardController *faqsController = [[StandardController alloc] initWithStyle:UITableViewStylePlain pager:NO];
     faqsController.model = [[FAQList alloc] init];
     faqsController.title = @"FAQs";
+    faqsController.tabBarItem.tag = kTabFAQs;
     faqsController.tabBarItem.image = [UIImage imageNamed:@"faqs.png"];
     UINavigationController *faqsNavController = [[UINavigationController alloc] initWithRootViewController:faqsController];
     faqsNavController.navigationBar.tintColor = [UIColor navigationBarTintColour];
-    [tabControllers addObject:faqsNavController];
+    [tabControllers setObject:faqsNavController forIntegerKey:kTabFAQs];
     
     // other events
     StandardController *otherEventsController = [[StandardController alloc] initWithStyle:UITableViewStylePlain pager:NO];
     otherEventsController.title = @"Other events";
+    otherEventsController.tabBarItem.tag = kTabOtherEvents;
     otherEventsController.tabBarItem.image = [UIImage imageNamed:@"other-events.png"];
     otherEventsController.model = [[OtherEvents alloc] init];
     UINavigationController *otherEventsNavController = [[UINavigationController alloc] initWithRootViewController:otherEventsController];
     otherEventsNavController.navigationBar.tintColor = [UIColor navigationBarTintColour];
-    [tabControllers addObject:otherEventsNavController];
+    [tabControllers setObject:otherEventsNavController forIntegerKey:kTabOtherEvents];
     
     // special offers
     StandardController *offersController = [[StandardController alloc] initWithStyle:UITableViewStylePlain pager:NO];
     offersController.title = @"Special offers";
+    offersController.tabBarItem.tag = kTabSpecialOffers;
     offersController.tabBarItem.image = [UIImage imageNamed:@"special-offers.png"];
     offersController.model = [[SpecialOfferList alloc] init];
     UINavigationController *offersNavController = [[UINavigationController alloc] initWithRootViewController:offersController];
     offersNavController.navigationBar.tintColor = [UIColor navigationBarTintColour];
-    [tabControllers addObject:offersNavController];
+    [tabControllers setObject:offersNavController forIntegerKey:kTabSpecialOffers];
+    
+    // how are tabs sorted?
+    NSArray *tabOrder = [[NSUserDefaults standardUserDefaults] objectForKey:kTabOrder];
+    if (tabOrder == nil) {
+        tabOrder = [NSArray arrayWithObjects: [NSNumber numberWithInt:kTabHome], [NSNumber numberWithInt:kTabSpeakers], [NSNumber numberWithInt:kTabProgramme], [NSNumber numberWithInt:kTabMaps], [NSNumber numberWithInt:kTabTwitter], [NSNumber numberWithInt:kTabDonate], [NSNumber numberWithInt:kTabAlerts], [NSNumber numberWithInt:kTabFAQs], [NSNumber numberWithInt:kTabOtherEvents], [NSNumber numberWithInt:kTabSpecialOffers], nil];
+    }
+    NSMutableArray *tabViewControllers = [NSMutableArray arrayWithCapacity:tabOrder.count];
+    for (NSNumber *tag in tabOrder) {
+        [tabViewControllers addObject:[tabControllers objectForKey:tag]];
+    }
     
     UITabBarController *tabController = [[UITabBarController alloc] init];
     tabController.moreNavigationController.navigationBar.tintColor = [UIColor navigationBarTintColour];
-    tabController.viewControllers = tabControllers;
+    tabController.viewControllers = tabViewControllers;
     self.tabBarController = tabController;
     self.tabBarController.delegate = self;
 
@@ -144,6 +177,17 @@
   id modalViewCtrl = [[[tabBarController view] subviews] objectAtIndex:1];  
   if([modalViewCtrl isKindOfClass:NSClassFromString(@"UITabBarCustomizeView")] == YES)
     ((UINavigationBar*)[[modalViewCtrl subviews] objectAtIndex:0]).tintColor = [UIColor navigationBarTintColour];
+}
+
+
+- (void)tabBarController:(UITabBarController *)tabBarController didEndCustomizingViewControllers:(NSArray *)viewControllers changed:(BOOL)changed {
+    if (changed) {
+        NSMutableArray *tabOrder = [NSMutableArray array];
+        for (UIViewController *c in viewControllers) {
+            [tabOrder addObject:[NSNumber numberWithInt:c.tabBarItem.tag]];
+        }
+        [[NSUserDefaults standardUserDefaults] setObject:tabOrder forKey:kTabOrder];
+    }
 }
 
 
